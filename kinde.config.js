8dc103cf32f8185e5585869c0eee9e32827bae9a
Aryangurung1 <aryan.gurung683@gmail.com>
2025-05-15 02:36:33 +0545

    kinde login debug

--- kinde.config.js
+++ kinde.config.js
@@ -2,6 +2,7 @@ module.exports = {
     cookies: {
         secure: false,      // Use false for HTTP (true for HTTPS)
         sameSite: 'lax',    // 'lax' is safe for HTTP, 'none' only for HTTPS
+        domain: '44.222.28.208', // <-- Your new public IP
         // domain: '54.221.61.214', // Optional, can help with IPs
     }
 }; 
